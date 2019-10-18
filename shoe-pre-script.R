b8b06c313cfbfa5a46f43a25c821e874bcf804ee
Teresa Fukuda <tlfukuda@ESM.UCSB.EDU>
2019-10-18 14:43:39 -0700

    made beginning of a script to upload pre survey csv and clean up column names

---
+++ shoe-pre-script.R
@@ -0,0 +1,18 @@
+########################################################
+## Pre-Survey Wear Test Data Management 
+## ... an attempt to import the data and make it into a usable form for eas[ier] analysis
+## by Teresa
+## because I'm bored
+##########################################################
+
+# Part I. Load packages and import data
+
+library(tidyverse) # hello tidyverse
+library(janitor) # load janitor to clean up dataframe
+library(lubridate) # load lubridate to work with dates and times
+
+# I made this so that you can download the data from the Google Drive as a csv and import directly into here, no edits
+
+#########################################################
+
+# Part II. 
