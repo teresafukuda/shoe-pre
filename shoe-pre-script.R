ef480e766086c5c27841ab5324bf046af207991d
Teresa Fukuda <tlfukuda@esm.ucsb.edu>
2019-10-20 14:08:08 -0700

    cleaned up and added some "To do" items at the bottom... for now this isn't very useful

--- shoe-pre-script.R
+++ shoe-pre-script.R
@@ -1,24 +1,23 @@
 ########################################################
 ## Pre-Survey Wear Test Data Management 
 ## ... an attempt to import the data and make it into a usable form for eas[ier] analysis
-## by Teresa
-## because I'm bored
+## by Teresa for FUTURE FOOTWEAR (the best)
+## 
 ##########################################################
 
 # Part I. Load packages and import data
 
-library(tidyverse) # hello tidyverse
+library(tidyverse) # load tidyverse
 library(janitor) # load janitor to clean up dataframe
-library(lubridate) # load lubridate to work with dates and times
 
 # I made this so that you can download the data from the Google Drive as a csv and import directly into here, no edits
 
-pre_survey <- read_csv("Pre Survey Data - Sheet1.csv") 
+pre_survey <- read_csv("Pre Survey Data - Sheet1.csv") # might have to change import filename here!!!
 
 
 #########################################################
 
-# Part II. Clean up data
+# Part II. Clean up data ### Going to need more work either here or in google docs to make data more useful for some questions (or we can igmore them altogether)
 
 clean_pre <- pre_survey  %>% 
   rename("name"= 'X1') %>% 
@@ -38,4 +37,8 @@ weight_vis <- ggplot(clean_pre, aes(x=weight)) +
 end_of_life_vis <- ggplot(clean_pre, aes(x=question_18)) + 
   geom_histogram(stat='count')
 
-end_of_life_vis
+
+########################################################
+
+# Might be interesting next to try to look at step data and combine with age/weight and see if there are any obvious trends
+
